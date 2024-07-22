c59b4d09ffdde407e0b7fa88fe8a596d062ee225
unknown <ashutoshsingh1711@gmail.com>
2024-07-22 19:08:21 +0530

    Added token creation and verification methods

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const { JWT_KEY } = require("../config/serverConfig");
 const UserRepository = require("../repository/user-repository");
 
 class UserService {
@@ -20,6 +22,26 @@ class UserService {
       return true;
     } catch (error) {
       console.log("Something went wrong in the service layer");
+      throw error;
+    }
+  }
+
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1d" });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in token creation");
+      throw error;
+    }
+  }
+
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in token validation");
       throw error;
     }
   }
