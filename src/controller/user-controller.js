01ad2fd35a05491156286500503229e852bb0596
unknown <ashutoshsingh1711@gmail.com>
2024-07-25 16:51:46 +0530

    Added isAdmin api

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -68,8 +68,32 @@ const isAuthenticated = async (req, res) => {
   }
 };
 
+const isAdmin = async (req, res) => {
+  try {
+    const response = await userService.isAdmin(req.body.userId);
+    return res.status(200).json({
+      data: response,
+      err: [],
+      success: true,
+      // message: `${response} ? Successfully fetched whether user is ADMIN : Successfully fetched whether user is not ADMIN`,
+      message: response
+        ? "Successfully fetched user is ADMIN."
+        : "Successfully fetched user is not ADMIN`",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong",
+      data: [],
+      success: false,
+      err: error,
+    });
+  }
+};
+
 module.exports = {
   create,
   signin,
   isAuthenticated,
+  isAdmin,
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
-
+const { User, Role } = require("./models/index");
 const apiRoutes = require("../src/routes/index");
 const bodyParser = require("body-parser");
 const db = require("./models/index");
@@ -11,11 +11,18 @@ const prepareAndStartServer = () => {
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use("/api", apiRoutes);
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server started at ${PORT}`);
     // if (process.env.DB_SYNC) {
     //   db.sequelize.sync({ alter: true });
     // }
+    const user = await User.findByPk(3);
+    const adminRole = await Role.findOne({
+      where: {
+        name: "ADMIN",
+      },
+    });
+    user.addRole(adminRole);
   });
 };
 
